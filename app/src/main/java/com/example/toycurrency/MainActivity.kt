5bd01a033ba348efc65a5e2addb17686f67dae38
Parkduksung <duksung1234@naver.com>
2022-12-15 21:30:37 +0900

    [feature] Use ListPicker

--- app/src/main/java/com/example/toycurrency/MainActivity.kt
+++ app/src/main/java/com/example/toycurrency/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+import com.chargemap.compose.numberpicker.ListItemPicker
 import com.example.toycurrency.ui.theme.ToyCurrencyTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ListPicker(listOf("KSW", "JPY", "PHP"))
                 }
             }
         }
@@ -30,14 +33,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun ListPicker(nation : List<String>) {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ToyCurrencyTheme {
-        Greeting("Android")
-    }
+    var state by remember { mutableStateOf(nation[0]) }
+
+    ListItemPicker(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        label = { it },
+        value = state,
+        onValueChange = {
+            state = it
+        }, list = nation,
+        dividersColor = MaterialTheme.colors.primary,
+        textStyle = TextStyle(fontSize = 20.sp)
+    )
 }
