ff1422518318597d196b39d5231da2a10e347a9e
Parkduksung <duksung1234@naver.com>
2022-12-18 23:57:40 +0900

    [feature] calendarExt 추가.

---
+++ app/src/main/java/com/example/toycurrency/ext/CalendarExt.kt
@@ -0,0 +1,9 @@
+package com.example.toycurrency.ext
+
+import android.annotation.SuppressLint
+import android.icu.text.SimpleDateFormat
+
+
+@SuppressLint("SimpleDateFormat")
+fun Long?.convertTimeString(pattern : String) : String =
+    SimpleDateFormat(pattern).format(this)
