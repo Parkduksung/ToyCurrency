b25f9f4bd5ce368cfb71d7512b76dccd3b365b03
Parkduksung <duksung1234@naver.com>
2022-12-18 23:57:14 +0900

    [feature] result 수정.

--- app/src/main/java/com/example/toycurrency/util/Result.kt
+++ app/src/main/java/com/example/toycurrency/util/Result.kt
@@ -1,21 +1,7 @@
 package com.example.toycurrency.util
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
-
-sealed interface Result<out T> {
-    data class Success<T>(val data: T) : Result<T>
-    data class Error(val exception: Throwable? = null) : Result<Nothing>
-    object Loading : Result<Nothing>
-}
-
-fun <T> Flow<T>.asResult(): Flow<Result<T>> {
-    return this
-        .map<T, Result<T>> {
-            Result.Success(it)
-        }
-        .onStart { emit(Result.Loading) }
-        .catch { emit(Result.Error(it)) }
+sealed class Result<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T) : Result<T>(data)
+    class Error<T>(message: String, data: T? = null) : Result<T>(data, message)
+    class Loading<T>(data: T? = null) : Result<T>(data)
 }
